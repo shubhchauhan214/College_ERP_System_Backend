d73af75d5c595c2ddf39833a76161d74842d00c2
shubhchauhan214 <shubhchauhan616@gmail.com>
2026-05-04 19:42:29 +0530

    User Schemas Created

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.database import Base, engine
+from app import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="College ERP Backend")
 
---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from datetime import date, datetime
+from decimal import Decimal
+from typing import Optional, List
+
+from pydantic import BaseModel, ConfigDict
+
+
+# ------------USER / AUTH ----------------
+class UserCreate(BaseModel):
+    full_name: str
+    username: str
+    passowrd: str
+    role: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    full_name: str
+    username: str
+    role: str
+    is_active: bool
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    user: UserOut
+
